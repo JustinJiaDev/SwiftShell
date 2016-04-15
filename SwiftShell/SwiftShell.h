d4e6980117a7ae470ad5ea3e8363e5bdc1953ea4
Justin Jia <justin.jia.developer@gmail.com>
2016-04-15 15:58:42 -0700

    Initial Commit

---
+++ SwiftShell/SwiftShell.h
@@ -0,0 +1,19 @@
+//
+//  SwiftShell.h
+//  SwiftShell
+//
+//  Created by Justin Jia on 4/15/16.
+//  Copyright © 2016 SwiftShell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftShell.
+FOUNDATION_EXPORT double SwiftShellVersionNumber;
+
+//! Project version string for SwiftShell.
+FOUNDATION_EXPORT const unsigned char SwiftShellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftShell/PublicHeader.h>
+
+
