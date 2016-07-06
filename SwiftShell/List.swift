ce5bb6352233ed46d07a58ed0a6655aabffa087f
Justin Jia <justin.jia.developer@gmail.com>
2016-07-06 00:52:50 -0500

    Rename ListOptions to LsOptions

--- SwiftShell/List.swift
+++ SwiftShell/List.swift
@@ -8,11 +8,11 @@
 
 import Foundation
 
-public enum ListOption: String {
+public enum LsOption: String {
     case all = "-a"
     case longFormat = "-l"
 }
 
-public func ls(_ options: ListOption...) -> String {
+public func ls(_ options: LsOption...) -> String {
     return run("ls", with: options.map { $0.rawValue })
 }
